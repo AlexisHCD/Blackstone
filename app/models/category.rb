22df30123e8a54c3f4760aacb83a696da67a41d5
Alexis Hernandez <adhcamus@gmail.com>
2026-04-18 13:07:25 -0400

    feat: add curated categories with descriptions and slug system

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,14 @@
 class Category < ApplicationRecord
   has_many :tools
 
+  before_validation :generate_slug
+
   validates :name, presence: true
   validates :slug, presence: true, uniqueness: true
+
+  private
+
+  def generate_slug
+    self.slug = name.parameterize if name.present?
+  end
 end
